d93498356b05fae0bd2aaaf60407fc413deb0c05
crixx-klap <123806030+crixx-klap@users.noreply.github.com>
2023-02-09 08:22:38 +0000

    Refactor

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,27 +1,135 @@
-import { useEffect, useState } from 'preact/hooks';
+import { useCallback, useEffect, useState } from 'preact/hooks';
 import style from './style.module.css';
 
 // Note: `user` comes from the URL, courtesy of our router
 export default function Trainer({ user }) {
-	const [time, setTime] = useState(Date.now());
-	const [count, setCount] = useState(10);
+	const randomCountdown = () => Math.random() > 0.7 ? 700 : 2500;
+	const waitingForInput = "waitingForInput";
+	const paused = "paused";
+	const matched = "matched";
+	const missed = "missed";
+	const timeout = "timeout";
 
-	useEffect(() => {
-		let timer = setInterval(() => setTime(Date.now()), 1000);
-		return () => clearInterval(timer);
+
+	const [keyPressed, setKeyPressed] = useState(null);
+	const [targetKey, setTargetKey] = useState(null);
+	const [timeoutId, setTimeoutId] = useState(0);
+	const [counter, setCounter] = useState(0);
+	const [hit, setHit] = useState(0);
+	const [miss, setMiss] = useState(0);
+	const [late, setLate] = useState(0);
+	const [status, setStatus] = useState(paused);
+
+	const delay = 1000;
+	const delayInS = '--duration: 1s;';
+	const numberOfRuns = 30;
+
+	const items = [
+		'3', '6', '9', '=',
+		'2', '5', '8', '-',
+		'1', '4', '7', '0'
+	];
+
+	const items2 = [
+		'q', 'w', 'e', 'r',
+		'a', 's', 'd', 'f',
+		'y', 'x', 'c', 'v'
+	];
+
+	useEffect(()=>{
+		console.log("run once")
+		window.addEventListener("keydown", keyPressHandler2);
+		return () => {
+			window.removeEventListener("keydown", keyPressHandler2);
+		};
 	}, []);
+
+	const onStartClick = () => {
+		setStatus(waitingForInput);
+		setCounter(0);
+		setHit(0);
+		setMiss(0);
+		setLate(0);
+	}
+
+	useEffect(()=>{
+		console.log(status, counter);
+		
+		if (status === waitingForInput) {
+			setTargetKey(items[Math.floor(Math.random() * items.length)]);
+			setTimeoutId(setTimeout(() => {
+				setStatus(timeout);
+			}, delay));
+		} else if (status === paused) {
+			clearTimeout(timeoutId);
+			setTargetKey(undefined);
+		} else {
+			clearTimeout(timeoutId);
+			
+			if (status === matched) {
+				setHit(x => x + 1);
+			} else if (status === missed) {
+				setMiss(x => x + 1);
+			} else if (status === timeout) {
+				setLate(x => x + 1);
+			}
+			if (counter + 1 >= numberOfRuns) {
+				setStatus(paused);
+			} else {
+				setStatus(waitingForInput);
+				setCounter(c => c + 1);
+			}
+		} 
+	}, [status]);
+
+	const keyPressHandler2 = (e) =>{
+		setKeyPressed(e);
+	};
+
+	useEffect(()=>{
+		if (status !== waitingForInput)
+			return;
+
+		console.log(keyPressed)
+
+		if (keyPressed.key === targetKey) {
+			setStatus(matched);
+		} else {
+			setStatus(missed);
+		}
+	}, [keyPressed]);
+
 
 	return (
 		<div class={style.profile}>
-			<h1>Profile: {user}</h1>
-			<p>This is the user profile for a user named {user}.</p>
-
-			<div>Current time: {new Date(time).toLocaleString()}</div>
-
-			<p>
-				<button onClick={() => setCount(count => count + 1)}>Click Me</button>{' '}
-				Clicked {count} times.
-			</p>
-		</div>
+			<button onClick={onStartClick}>Start</button>
+			<h1>Status: {status}</h1>
+			<h3>hit: {hit}</h3>
+			<h3>miss: {miss}</h3>
+			<h3>late: {late}</h3>
+			<div class={style.grid}>
+				{items.map((x, index) => {
+					let c;
+					if (status === waitingForInput && targetKey === items[index]) {
+						c = style.circle;
+					}
+					let m;
+					if (status === missed && keyPressed.key === items[index]) {
+						m = style.missed;
+					}
+					return (
+						<div class={m} key={index}>
+							{c ? 
+							<svg width="100%" viewBox="0 0 100 100" className={c} style={delayInS}>
+								<circle cx="50" cy="50" r="40" stroke="#428bca" stroke-width="6" fill="#555" />
+							</svg> : 
+							<svg width="100%" viewBox="0 0 100 100">
+								<circle cx="50" cy="50" r="40" stroke="#333" stroke-width="6" fill="#333" />
+							</svg>}
+						</div>
+					)
+				})}
+			</div>
+		</div>		
 	);
 }
--- src/routes/trainer/index.js
+++ src/routes/trainer/index.js
@@ -3,59 +3,92 @@ import style from './style.module.css';
 
 // Note: `user` comes from the URL, courtesy of our router
 export default function Trainer({ user }) {
-	const randomCountdown = () => Math.random() > 0.7 ? 500 : 2500;
+	const randomCountdown = () => Math.random() > 0.7 ? 700 : 2500;
 	
-	const [keyPressed, setKeyPressed] = useState();
-	const [targetKey, setTargetKey] = useState();
+	const [keyPressed, setKeyPressed] = useState(null);
+	const [targetKey, setTargetKey] = useState(null);
 	const [timeoutId, setTimeoutId] = useState(0);
-	const [counter, setCounter] = useState(0);
-	const [countdown, setCountdown] = useState(randomCountdown());
+	const [counter, setCounter] = useState(-1);
+	const [hit, setHit] = useState(-1);
+	const [miss, setMiss] = useState(-1);
+	const [late, setLate] = useState(-1);
+
+	const delay = 700;
+
 
 	const items = [
-		'3', '6', '9', '=',
-		'2', '5', '8', '-',
-		'1', '4', '7', '0'
+		'q', 'w', 'e', 'r',
+		'a', 's', 'd', 'f',
+		'y', 'x', 'c', 'v'
 	];
 
 	const onStartClick = () => {
-		setCounter(0);
-		startCountDown();
-	}
-
-	const startCountDown = () => {
-		if (counter > 5) {
+		setTimeout(() => {
 			setCounter(0);
-			clearTimeout(timeoutId);
-			return;
-		}
-		console.log('countdown starting for the next item');
-		console.log(counter)
-		setTargetKey(items[Math.floor(Math.random() * items.length)]);
-		setCounter(x => x + 1);
-		setTimeoutId(setTimeout(() => { 
-			console.log('fail timeout');
-			//onStartClick();
-		}, 3000));
+			setHit(0);
+			setMiss(0);
+			setLate(0);			
+		}, 2000);
 	}
 
 	const keyPressHandler2 = ({ key }) =>{
-		console.log('key press handler');
-		console.log(timeoutId);
-		console.log(targetKey);
-		console.log(counter);
-		clearTimeout(timeoutId);
-		if(targetKey === key){
-			console.log('hit', targetKey, key);
-		} else {
-			console.log('miss', targetKey, key);
-		}
+		if (counter < 0) 
+			return;
 		setKeyPressed(key);
-		startCountDown();
+		setCounter(x => x + 1);
 	};
 
 	useEffect(() => {
 		console.log('use effect');
-		window.addEventListener("keydown", keyPressHandler2);
+		console.log(keyPressed);
+		console.log(counter);
+
+		if (counter === 0){
+			setTimeoutId(setTimeout(() => {
+				console.log('fail timeout');
+				setLate(x => x + 1);
+				setKeyPressed(null);
+				setCounter(x => x + 1);
+			}, delay));
+
+			setTargetKey(items[Math.floor(Math.random() * items.length)]);
+		} else if (counter > 0) {
+			if (keyPressed) {
+				if (keyPressed === targetKey) {
+					console.log('hit', keyPressed, targetKey);
+					setHit(x => x + 1);
+				} else {
+					console.log('miss', keyPressed, targetKey);
+					setMiss(x => x + 1);				
+				}	
+			}
+
+			if (timeoutId) {
+				clearTimeout(timeoutId);
+			}
+
+			if (counter === 20) {
+				setCounter(-1)
+				setTargetKey(null);
+				setKeyPressed(null);
+			} else {
+				setTimeoutId(setTimeout(() => {
+					console.log('fail timeout');1
+					setLate(x => x + 1);
+					setKeyPressed(null);
+					setCounter(x => x + 1);
+				}, delay));
+	
+				setTargetKey(items[Math.floor(Math.random() * items.length)]);
+				setKeyPressed(null);
+			}
+		}
+
+		if (counter !== -1)
+			window.addEventListener("keydown", keyPressHandler2);
+		else 
+			window.removeEventListener("keydown", keyPressHandler2);
+		
 		return () => {
 			window.removeEventListener("keydown", keyPressHandler2);
 		};
@@ -65,9 +98,26 @@ export default function Trainer({ user }) {
 		<div class={style.profile}>
 			<button onClick={onStartClick}>Start</button>
 			<h1>Counter: {counter}</h1>
+			<h3>hit: {hit}</h3>
+			<h3>miss: {miss}</h3>
+			<h3>late: {late}</h3>
 			<div class={style.grid}>
 				{items.map((x, index) => {
-					return <div class={(x === targetKey ? style.selected : "")} key={index}>{x}</div>
+					let c = '';
+					if (keyPressed === null && targetKey !== null && x === targetKey){
+						c = style.selected;
+					} else if (keyPressed !== null && targetKey !== null && keyPressed !== targetKey && keyPressed === x) {
+						c = style.missed;
+					}
+					return (
+						<div class={c} key={index}>
+							<svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+								<g class={style['base-timer-circle']}>
+									<circle class={style['base-timer-path-elapsed']} cx="50" cy="50" r="45" />
+								</g>
+							</svg>
+						</div>
+					)
 				})}
 			</div>
 		</div>
